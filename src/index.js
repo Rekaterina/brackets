295001c621451cbd49b62e6097044ea380501352
Rekaterina <rkatherine0602@gmail.com>
2019-10-04 18:41:17 +0300

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let arr = [];
+      for (let i = 0; i < bracketsConfig.length; i++) {
+         arr.push(bracketsConfig[i][0] + bracketsConfig[i][1]);
+      }
+      
+      for (l = 0; l < 5; l++) {
+        for (let j = 0; j < arr.length; j++) {
+          while (str.indexOf(arr[j]) >= 0) {
+            str = str.replace(arr[j], '');
+          }
+        }
+      }
+       
+      if (str.length == 0) {
+        return true;
+      } else {
+         return false;
+      }
+  }
